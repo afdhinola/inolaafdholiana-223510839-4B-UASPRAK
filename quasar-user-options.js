307110eebf64937f31350ecb7521401d2a60409f
Inola Afdholiana <156224232+afdhinola@users.noreply.github.com>
2024-06-26 22:01:21 +0700

    Add files via upload

---
+++ quasar-user-options.js
@@ -0,0 +1,12 @@
+import 'quasar/dist/quasar.css';
+import iconSet from 'quasar/icon-set/fontawesome-v5';
+import lang from 'quasar/lang/en-us';
+import '@quasar/extras/material-icons/material-icons.css';
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css';
+
+export default {
+  config: {},
+  plugins: {},
+  lang: lang,
+  iconSet: iconSet
+};
